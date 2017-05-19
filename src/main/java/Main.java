9377e814c01e7523c79c60b770f080b104194a3c
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2017-05-19 21:54:27 +0200

    Randomwalks are possible, from a given startingpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.rdfhdt.hdt.triples.TripleString;
 
 import java.util.Iterator;
 import java.util.Random;
+import java.util.ArrayList;
 import static java.lang.Math.toIntExact;
 
 /**
@@ -14,46 +15,47 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Load HDT file NOTE: Use loadIndexedHDT() if you are doing ?P?, ?PO, ??O queries
         HDT hdt = HDTManager.loadHDT("../mappingbased-dbpedia.en.2015-10.hdt", null);
-
-        CharSequence newNode = "http://dbpedia.org/resource/Kiss_Kiss_Bang_Bang";
-//        newNode = "http://dbpedia.org/resource/Category:Screenplays_by_Shane_Black";
-        IteratorTripleString it = hdt.search(newNode, "", "");
-        System.out.println("Estimated number of results: "+it.estimatedNumResults());
         Random rand = new Random();
-        int next = rand.nextInt(toIntExact(it.estimatedNumResults()));
+        
+    	CharSequence newNode = "http://dbpedia.org/resource/Kiss_Kiss_Bang_Bang";
+    	
         int j = 0;
+        ArrayList<CharSequence> randomWalk = new ArrayList<CharSequence>();
+        CharSequence predicate = "";
+        IteratorTripleString it = hdt.search(newNode.toString(), "", "");
+        randomWalk.add(newNode);
+    	
         while(j < 8){
+        	j++;
+
+        	try{
+				it = hdt.search(newNode.toString(), "", "");
+	            System.out.println("Estimated number of results: "+it.estimatedNumResults()+" --- "+newNode.toString());
+        	}catch (org.rdfhdt.hdt.exceptions.NotFoundException e){
+        		System.out.println(newNode.toString() + " has zero outgoing edges.");
+        		randomWalk.remove(newNode.toString());
+        		randomWalk.remove(predicate);
+        	}
+            
+            int next = rand.nextInt(toIntExact(it.estimatedNumResults()));
             int i = 0;
+            it.goToStart();
         	while(it.hasNext()) {
 	        	i++;
 	            TripleString ts = it.next();
-	//            System.out.println(ts);
 	            if(i == next){
+	            	predicate = ts.getPredicate().toString();
 	            	newNode = ts.getObject();
+	        		randomWalk.add(predicate);
+	        		randomWalk.add(newNode.toString());
+	                System.out.print("\nTrying ");
+	                System.out.println(newNode);
 	            }
 	        }
-        	j++;
-        	System.out.println(newNode);
-        	try{
-        		it = hdt.search(newNode.toString(), "", "");
-        	}catch (org.rdfhdt.hdt.exceptions.NotFoundException e){
-        		System.out.println("Not found.");
-        		int old_next = next;
-        		while (old_next == next){
-                    next = rand.nextInt(toIntExact(it.estimatedNumResults()));
-        		}
-        	}
-            System.out.println("Estimated number of results: "+it.estimatedNumResults());
-            next = rand.nextInt(toIntExact(it.estimatedNumResults()));
-            System.out.println("----");
         }
-
-//        // List all predicates
-//        System.out.println("Dataset contains "+hdt.getDictionary().getNpredicates()+" predicates:");
-//        Iterator<? extends CharSequence> itPred = hdt.getDictionary().getPredicates().getSortedEntries();
-//        while(itPred.hasNext()) {
-//            CharSequence str = itPred.next();
-//            System.out.println(str);
-//        }
+        System.out.println("\nThe randomwalk:");
+        for(int i = 0; i < randomWalk.size(); i++){
+            System.out.println(randomWalk.get(i));	
+        }
     }
 }
