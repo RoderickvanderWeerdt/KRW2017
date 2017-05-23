da9f404823050d5eb6eb4ae8204bc63a7af8eea6
Celeste Kettler <celestekettler@gmail.com>
2017-05-23 17:14:31 +0200

    word2vec in combination with svm on train and testset

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,5 +1,30 @@
 import word2vec as w2v
+import tsv_reader as tsvr
+import svm
 
-filepath = "../../../../superSmallRandomWalks.txt"
-word2vec_creator = w2v.word2vec(filepath)
-word_vectors = word2vec_creator.create_wordvectors()
+filepath_random_walks = "../../../../superSmallRandomWalks.txt"
+filepath_movies_train = "../../../../mc-movies.pruned.train.tsv"
+filepath_movies_test = "../../../../mc-movies.pruned.test.tsv"
+
+def prepare_for_svm(tsv_file):
+    tsv_reader = tsvr.tsvReader(filepath_movies_train)
+    words, labels = tsv_reader.retrieve_words_and_labels()
+    word2vec_creator = w2v.word2vec(filepath_random_walks, words, labels)
+    word_vectors, words, labels = word2vec_creator.create_wordvectors()
+    return word_vectors, words, labels
+
+def train_setup(filepath_train):
+    word_vectors, words, labels = prepare_for_svm(filepath_train)
+    classifier = svm.svmClassifier(word_vectors, labels)
+    classifier.create_svm_model()
+
+def test_setup(filepath_test):
+    word_vectors, words, labels = prepare_for_svm(filepath_test)
+    classifier = svm.svmClassifier(word_vectors, labels)
+    classifier.validate_svm_model()
+
+def main():
+    train_setup(filepath_movies_train)
+    test_setup(filepath_movies_test)
+
+if __name__ == "__main__": main()
--- src/main/python/word2vec.py
+++ src/main/python/word2vec.py
@@ -1,17 +1,31 @@
 import gensim.models.word2vec as genw2v
 
 class word2vec:
-    def __init__(self, filepath):
+    def __init__(self, filepath, words, labels):
         self.filepath = filepath
+        self.words = words
+        self.labels = labels
+
+    def to_wordvector_list(self, model):
+        word_vectors = []
+        new_words_list = []
+        new_labels_list = []
+        for index, word in enumerate(self.words):
+            # try every word in the model, if model does not have this word as a key it is not used for SVM
+            try:
+                word_vectors.append(model[word])
+                new_words_list.append(word)
+                new_labels_list.append(self.labels[index])
+            except KeyError:
+                print("Word in labeled file but not in model (random paths): " + word)
+        self.words = new_words_list
+        self.labels = new_labels_list
+        return word_vectors
 
     def create_wordvectors(self):
-        #line_sentence = Simple format: one sentence = one line; words already preprocessed and separated by whitespace.
+        # line_sentence = Simple format: one sentence = one line;
+        # words already preprocessed and separated by whitespace.
         sentences = genw2v.LineSentence(self.filepath)
-        # To load an existing model:
-        # model = Word2Vec.load("../w2v_model")
         model = genw2v.Word2Vec(sentences, size=100, window=5, min_count=1, workers=4)
-        # To save the current model:
-        # model.save("../w2v_model")
-        word_vectors = model.wv
-        print(word_vectors)
-        return word_vectors
+        word_vector_list = self.to_wordvector_list(model)
+        return word_vector_list, self.words, self.labels
