0f5245b579435fddd70cc0d5d1d6cd83c20fe396
Celeste Kettler <celestekettler@gmail.com>
2017-06-01 14:40:01 +0200

    uncovered and fixed some faults in the implementation

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -7,26 +7,30 @@ filepath_random_walks = "random_walks_api.txt"
 filepath_movies_train = "../../../../mc-movies.pruned.train.tsv"
 filepath_movies_test = "../../../../mc-movies.pruned.test.tsv"
 
-def prepare_for_svm(tsv_file):
-    tsv_reader = tsvr.tsvReader(filepath_movies_train)
+
+def prepare_for_svm(tsv_file, phase):
+    tsv_reader = tsvr.tsvReader(tsv_file)
     words, labels = tsv_reader.retrieve_words_and_labels()
-    word2vec_creator = w2v.word2vec(filepath_random_walks, words, labels)
+    word2vec_creator = w2v.word2vec(filepath_random_walks, words, labels, phase)
     word_vectors, words, labels = word2vec_creator.create_wordvectors()
     return word_vectors, words, labels
 
+
 def train_setup(filepath_train):
     print("[Creating word vectors....]")
-    word_vectors, words, labels = prepare_for_svm(filepath_train)
+    word_vectors, words, labels = prepare_for_svm(filepath_train, 'train')
     print("[Training model....]")
     classifier = svm.svmClassifier(word_vectors, labels)
     classifier.create_svm_model()
 
+
 def test_setup(filepath_test):
     print("[Creating word vectors....]")
-    word_vectors, words, labels = prepare_for_svm(filepath_test)
+    word_vectors, words, labels = prepare_for_svm(filepath_test, 'test')
     print("[Testing model....]")
     classifier = svm.svmClassifier(word_vectors, labels)
     classifier.validate_svm_model()
+
 
 def main():
     start_time = time.time()
@@ -40,5 +44,6 @@ def main():
     print("[Time elapsed testing: %s]" % test_time)
     total_time = time.time() - start_time
     print("[Total runtime of program: %s]" % total_time)
+
 
 if __name__ == "__main__": main()
